e227a1bc46e92f1361a79b325f445cfb25e9385d
Prasanth138 <prasanthanbu777@gmail.com>
2022-09-14 06:34:35 +0530

    first commit

---
+++ src/pages/Order/index.js
@@ -0,0 +1,192 @@
+import React, { useEffect, useState } from 'react';
+import {Button, Card, CardContent, CircularProgress, Container, Divider, Grid } from '@material-ui/core';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { Alert } from '@material-ui/lab';
+import { useDispatch, useSelector } from 'react-redux';
+import { deliverOrder, getOrderDetails, payOrder } from '../../actions/orderActions';
+import Loader from '../../components/Loader/Loader';
+import { Message, OrderItem, ShippingMessage, SummaryItem, StyledLink } from './Order.elements';
+import axios from 'axios';
+import types from '../../actions/types';
+import Meta from '../../components/Meta';
+import { clearCart } from '../../actions/cartActions';
+
+const Order = ({match, history}) => {
+
+    const orderId = match.params.id;
+
+    const [ sdkReady, setSdkReady ] = useState(false);
+
+    const currentUser = useSelector( state => state.currentUser);
+    const { userInfo } = currentUser;
+
+    const orderDetails = useSelector( state => state.orderDetails); 
+    const { order, loading, error } = orderDetails;
+
+    const orderPay = useSelector( state => state.orderPay); 
+    const { success:successPay, loading:loadingPay} = orderPay;
+
+    const orderDeliver = useSelector( state => state.orderDeliver); 
+    const { success:successDeliver } = orderDeliver;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        
+        if(!userInfo || !userInfo.name){
+            history.push('/login')
+        }
+        dispatch({ type: types.ORDER_CREATE_RESET })
+        const addPayPalScript = async () => {
+            const {data: clientId } = await axios.get('/api/config/paypal');
+            const script = document.createElement('script');
+
+            script.type = 'text/javascript';
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+            script.async = true;
+            script.addEventListener('load', ()=>{setSdkReady(true)})
+            document.body.appendChild(script);
+        }
+
+        if(!order || order._id !== orderId || successPay || successDeliver){
+            dispatch({ type: types.ORDER_PAY_RESET })
+            dispatch({ type: types.ORDER_DELIVER_RESET })
+            dispatch(getOrderDetails(orderId))
+            dispatch(clearCart());
+        } else if (!order.isPaid){
+
+            if(!window.paypal){
+                addPayPalScript()
+            }else {
+                setSdkReady(true)
+            }
+        }
+
+    }, [history, userInfo, order, orderId, successPay, successDeliver, dispatch])
+
+    const successPaymentHandler = (paymentResult) => {
+        dispatch(payOrder(orderId, paymentResult));
+        
+    }
+
+    const deliverHandler = () => {
+        dispatch(deliverOrder(orderId))
+    }
+
+    return (
+        <div className='order-page'>
+            <Meta title="Your Order | Book Store" />
+            <Container maxWidth={'lg'}>
+            { loading ? <Loader /> : 
+            error ? <Alert severity='error'>{error}</Alert> :
+            <>
+            <h2>{`ORDER NO. ${order._id}`}</h2>
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={8}>
+                    <h3>SHIPPING</h3>
+                    <p>
+                        <strong>Name: </strong>
+                        {`${order.user.name}`
+                        }
+                    </p>
+                    <p>
+                        <strong>Email: </strong>
+                        {`${order.user.email}`
+                        }
+                    </p>
+                    <p>
+                        <strong>Address: </strong>
+                        {`${order.shippingAddress.address}, ${order.shippingAddress.city}, ${order.shippingAddress.postalCode} ${order.shippingAddress.country}`
+                        }
+                    </p>
+                    <Message>
+                    {order.isDelivered ? 
+                        <Alert severity='success'>Delivered on {order.deliveredAt}</Alert> :
+                        <Alert
+                        severity='error'>
+                            Not Delivered Yet
+                        </Alert>
+                    }
+                    </Message>
+                    <Divider />
+                    <h3>PAYMENT</h3>
+                    <p>
+                        <strong>Method: </strong>
+                        {order.paymentMethod}
+                    </p>
+                    <Message>
+                    {order.isPaid ? 
+                        <Alert severity='success'>Paid on {order.paidAt.slice(0,10)}</Alert> :
+                        <Alert
+                        severity='error'>
+                            Not Paid
+                        </Alert>
+                    }
+                    </Message>
+                    <Divider />
+                    <h3>Order Items</h3>
+                    {
+                        order.orderItems.length === 0 ? <Alert>No Items to Display</Alert> :
+                        
+                            order.orderItems.map( item => (
+                                <div key={item._id}>
+                                <OrderItem>
+                                    <img style= {{width: '50px'}} src={item.image} alt={item.name} />
+                                    <p>{item.name}</p>
+                                    <p>{`${item.qty} x ${item.price} = ${item.qty*item.price}`}</p>
+                                </OrderItem>
+                                <Divider />
+                                </div>
+                                ))
+                    }
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Container>
+                    <Card>
+                        <CardContent>
+                        <h3>ORDER SUMMARY</h3>
+                        <Divider/>
+                        <SummaryItem>Items:  <strong>${order.orderItems.reduce( (acc,item) => acc + item.price * item.qty , 0).toFixed(2)}</strong>
+                        </SummaryItem>
+                        <SummaryItem>Shipping:  <strong>${order.shippingPrice}</strong></SummaryItem>
+                        <SummaryItem>Total:  <strong>${order.totalPrice}</strong></SummaryItem>
+                        <ShippingMessage>(Orders above $50 have free shipping)</ShippingMessage>
+                        </CardContent>
+                        
+                            {!order.isPaid ? (
+                            <div>
+                                {loadingPay && <CircularProgress/>}
+                                {sdkReady ? (
+                                    <>
+                                    <PayPalButton amount={order.totalPrice}
+                                    onSuccess={successPaymentHandler}/>
+                                        <p style={{margin: '0 5px', fontSize: '12px'}}>Test Email: <strong>sb-7eglp6046533@personal.example.com</strong></p>
+                                        <p style={{margin: '5px 5px', fontSize: '12px'}}>Test Password: <strong>EscD8'j.</strong></p>
+                                    </>
+                                ) :
+                                <CircularProgress/>}
+                                </div>
+                            ):
+                            <StyledLink to='/'>
+                                <Button variant='contained' color='primary'>Continue Shopping</Button>
+                            </StyledLink>
+                            }
+                            {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                                <Button variant='contained' color='primary' onClick={deliverHandler}>
+                                    Mark As Delivered
+                                </Button>
+                            )}
+                        
+                    </Card>
+                    </Container>
+                </Grid>
+            </Grid>
+            </>
+            
+            }
+            </Container>
+        </div>
+    )
+}
+
+export default Order;
---
+++ src/pages/Payment/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Button, Container, FormControl, FormControlLabel, Paper, Radio, RadioGroup } from '@material-ui/core';
+import { FormContainer } from './Payment.elements';
+import { useDispatch, useSelector } from 'react-redux';
+import { savePaymentMethod } from '../../actions/cartActions';
+import StepperNav from '../../components/StepperNav';
+import Meta from '../../components/Meta';
+
+
+const PaymentPage = ({ history }) => {
+    
+    const cart = useSelector(state => state.cart);
+    const {shippingAddress} = cart
+
+    if(!shippingAddress){
+        history.push('/shipping')
+    }
+
+    const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(savePaymentMethod(paymentMethod));
+        history.push('/placeorder');
+    }
+
+    return (
+        <div className='shipping-page'>
+            <Meta title="Payment | Book Store" />
+            <Container maxWidth={'lg'} >
+            <StepperNav stepNumber={2}/>
+            <FormContainer component={Paper} justify='left' maxWidth='xs'>
+                <h2>PAYMENT METHOD</h2>
+                <form onSubmit={submitHandler}>
+                    <FormControl component='fieldset'>
+                        <RadioGroup value={paymentMethod} onChange={ e => setPaymentMethod(e.target.value)}>
+                            <FormControlLabel value='PayPal' control={<Radio/>} label='PayPal or Credit Card'/>
+                        </RadioGroup>
+                    </FormControl>
+                <Button variant='contained' color='primary' type='submit'>Continue</Button>
+                </form>
+
+            </FormContainer>
+            </Container>
+        </div>
+    )
+}
+
+export default PaymentPage;
